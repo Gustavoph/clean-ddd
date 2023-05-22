59f41e3b0da742874ed3e30e0362b1658432481b
Gustavoph <gusta.coliveira07@gmail.com>
2023-05-22 09:09:14 -0300

    feat: add either errorn handling

--- src/domain/forum/application/use-cases/delete-answer.ts
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -1,3 +1,4 @@
+import { Either, left, right } from '@/core/errors/either'
 import { AnswersRepository } from '../repositories/answers-repository'
 
 interface DeleteAnswerUseCaseRequest {
@@ -5,7 +6,7 @@ interface DeleteAnswerUseCaseRequest {
   answerId: string
 }
 
-interface DeleteAnswerUseCaseResponse {}
+type DeleteAnswerUseCaseResponse = Either<string, {}>
 
 export class DeleteAnswerUseCase {
   constructor(private answersRepository: AnswersRepository) {}
@@ -17,15 +18,15 @@ export class DeleteAnswerUseCase {
     const answer = await this.answersRepository.findById(answerId)
 
     if (!answer) {
-      throw new Error('Answer not found.')
+      return left('Answer not found.')
     }
 
     if (authorId !== answer.authorId.toString()) {
-      throw new Error('Not allowed.')
+      return left('Not allowed.')
     }
 
     await this.answersRepository.delete(answer)
 
-    return {}
+    return right({})
   }
 }
